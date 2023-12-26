30bf65ee93255a4220dc585e32c809b9e3dc583a
Ēriks Lapiņš <lapinseriks98@gmail.com>
2023-12-26 22:34:51 +0200

    starting to fix module api

--- components/Module/ModuleForm.vue
+++ components/Module/ModuleForm.vue
@@ -3,43 +3,6 @@
     <form ref="taskForm" class="grid gap-6 items-start w-full" @submit.prevent="createModuleItem">
       <div class="flex flex-col md:flex-row gap-6">
         <div class="w-full md:w-2/3 flex flex-col gap-6">
-          <div class="flex flex-col gap-6">
-            <UiSelect
-              v-model:model-value="form.activityTypeId"
-              :items="options"
-              name="activity-type"
-              label="Activity Type"
-              class="w-full md:w-1/2"
-            />
-            <div class="flex flex-col md:flex-row gap-6">
-              <UiDateSelect
-                v-model:model-value="form.startDate"
-                name="start-date"
-                label="Start date"
-                class="w-full md:w-1/2"
-              />
-              <UiDateSelect
-                v-model:model-value="form.dueDate"
-                name="due-date"
-                label="Due date"
-                class="w-full md:w-1/2"
-              />
-            </div>
-          </div>
-          <div class="flex flex-col md:flex-row gap-6">
-            <UiCheckbox
-              v-model:model-value="form.done"
-              name="is-done"
-              label="Done"
-              class="w-full md:w-1/2"
-            />
-            <UiDateSelect
-              v-model:model-value="form.doneOn"
-              name="done-on"
-              label="Done on"
-              class="w-full md:w-1/2"
-            />
-          </div>
           <UiTextInput
             v-model:model-value="form.title"
             name="title"
@@ -52,44 +15,6 @@
             label="Description"
             class="h-[300px]"
             @update:model-value="value => form.description = value"
-          />
-        </div>
-        <div class="w-full md:w-1/3 flex flex-col gap-6">
-          <UiSelect
-            v-model:model-value="form.managerId"
-            :items="userOptions"
-            name="manager-id"
-            label="Manager"
-          />
-          <UiSelect
-            v-model:model-value="form.quoteId"
-            :items="options"
-            name="quote-id"
-            label="Quote No"
-          />
-          <UiSelect
-            v-model:model-value="form.invoiceId"
-            :items="options"
-            name="invoice-id"
-            label="Invoice No"
-          />
-          <UiSelect
-            v-model:model-value="form.companyId"
-            :items="options"
-            name="company-id"
-            label="Company"
-          />
-          <UiSelect
-            v-model:model-value="form.personId"
-            :items="options"
-            name="person-id"
-            label="Person"
-          />
-          <UiSelect
-            v-model:model-value="form.projectId"
-            :items="options"
-            name="project-id"
-            label="Project No"
           />
         </div>
       </div>
@@ -107,7 +32,7 @@
 </template>
 
 <script setup lang="ts">
-import type { Tasks } from '@prisma/client';
+import type { ModuleItems, Tasks } from '@prisma/client';
 import { useUserStore } from '~/store/userStore';
 
 defineOptions({
@@ -120,8 +45,12 @@ const props = defineProps({
     default: undefined
   },
   itemDetails: {
-    type: Object as PropType<Tasks>,
+    type: Object as PropType<ModuleItems>,
     default: undefined
+  },
+  module: {
+    type: String,
+    default: ''
   }
 });
 
@@ -131,26 +60,15 @@ const errors = ref<Record<string, any>>({
 
 const userStore = useUserStore();
 
-const form = ref<Omit<Tasks, 'id' | 'clientId'>>({
+const form = ref<Omit<ModuleItems, 'id'>>({
   createdOn: new Date(),
   updatedOn: new Date(),
-  createdById: '',
-  updatedById: '',
-  // @ts-ignore
-  startDate: null,
-  dueDate: null,
-  done: false,
-  // @ts-ignore
-  doneOn: null,
-  managerId: null,
-  quoteId: null,
-  invoiceId: null,
-  companyId: null,
-  personId: null,
-  projectId: null,
+  createdById: 0,
+  updatedById: 0,
   title: '',
   description: '',
-  activityTypeId: null
+  module: props.module,
+  data: {}
 });
 
 onBeforeMount(() => {
@@ -163,47 +81,23 @@ onBeforeMount(() => {
 
 const userOptions = ref<{key: string, title: string}[]>([]);
 
-const options = computed(() => {
-  return [
-    {
-      title: 'something',
-      key: '1'
-    },
-    {
-      title: 'something2',
-      key: '2'
-    },
-    {
-      title: 'somethin',
-      key: '3'
-    }
-  ];
-});
-
-watch(() => form.value, (newValue) => {
-  if (!newValue.done) {
-    newValue.doneOn = null;
-  } else if (newValue.doneOn === null) {
-    // @ts-ignore
-    form.value.doneOn = new Date().toISOString();
-  }
-}, { deep: true, immediate: true });
-
 async function createModuleItem () {
   if (props.itemId) {
     try {
-      const { data } = await $fetch('/api/data/tasks/update', {
+      const { data } = await $fetch(`/api/data/${props.module}/update`, {
         method: 'POST',
         body: {
           ...form.value,
           id: props.itemId,
-          clientId: userStore.currentCompany,
           updatedById: userStore.currentUserId,
-          updatedOn: new Date().toISOString()
+          updatedOn: new Date().toISOString(),
+          title: form.value.title,
+          description: form.value.description,
+          module: props.module
         }
       });
 
-      form.value = data as unknown as Tasks;
+      form.value = data as unknown as ModuleItems;
     } catch (e: any) {
       console.log(e);
       errors.value = e.data.data.errors;
--- pages/datasets/[module]/create/index.vue
+++ pages/datasets/[module]/create/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <TaskForm />
+    <ModuleForm />
   </div>
 </template>
 
