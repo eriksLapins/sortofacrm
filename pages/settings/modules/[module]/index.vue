6d41c6eedaea3a947b2f4f2561ce207e3ac2e778
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-01-06 22:52:25 +0200

    fixed not loading data

--- pages/settings/modules/[module]/index.vue
+++ pages/settings/modules/[module]/index.vue
@@ -2,40 +2,40 @@
   <div class="flex flex-col gap-4">
     <div class="w-full flex justify-between">
       <h1 class="text-l font-bold">
-        Modules
+        Module - {{ $route.params.module }}
       </h1>
       <UiButton
-        href="/settings/modules/add"
-        text="Add new"
+        :href="`/settings/modules/${$route.params.module}/edit`"
+        text="Edit"
         as-link-button
       />
     </div>
     <div class="separator" />
     <ClientOnly>
-      <table v-if="availableModules.length">
+      <table v-if="availableFields.length">
         <thead>
           <tr>
             <td class="font-bold text-base-plus text-center w-12">
               ID
             </td>
             <td class="font-bold text-base-plus">
-              Name
+              Title
             </td>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in availableModules" :key="item.key" class="hover:bg-gray-text-disabled hover:bg-opacity-30">
+          <tr v-for="item in availableFields" :key="item.key" class="hover:bg-gray-text-disabled hover:bg-opacity-30">
             <TableData class="text-center w-12" :to="`/settings/modules/${item.key}`">
               {{ item.id }}
             </TableData>
             <TableData :to="`/settings/modules/${item.key}`">
-              {{ item.name }}
+              {{ item.title }}
             </TableData>
           </tr>
         </tbody>
       </table>
       <div v-else class="font-bold text-md">
-        No modules found
+        No fields found
       </div>
       <template #fallback>
         <LoadingAnimation />
@@ -47,15 +47,18 @@
 <script setup lang="ts">
 
 defineOptions({
-    name: 'SettingsModules'
+    name: 'SettingsModuleView'
 });
 
 definePageMeta({
     layout: 'settings'
 });
 
+const route = useRoute();
+const currentModule = route.params.module;
+
 const { data } = useAsyncData(async () => {
-    const { data } = await $fetch('/api/data/modules/get');
+    const { data } = await $fetch(`/api/data/${currentModule}/field/get`);
 
     const jsonResponse = jsonParse(data);
 
@@ -64,9 +67,10 @@ const { data } = useAsyncData(async () => {
     };
 });
 
-const availableModules = computed(() => {
+const availableFields = computed(() => {
     return data.value?.jsonResponse || [];
 });
+
 </script>
 
 <style scoped>
